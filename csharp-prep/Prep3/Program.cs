ed683034cf4bdd9bea039a08505faac09ec23edf
Swingfighter@gmail.com <swingfighter@gmail.com>
2023-09-20 21:36:18 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumberGenerator = new Random();
+        int magic = randomNumberGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != magic)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            Console.WriteLine(guess);
+            if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
